08a1972ea9a1f9f7b4f9adcba8ca9d3d932e091c
mben23 <bmarisescu@gmail.com>
2024-02-13 18:46:20 +0200

    test github

---
+++ modul2/exercitii acasa 2.py
@@ -0,0 +1,22 @@
+# is_hot = True
+# is_cold = False
+#
+# if is_hot:
+#     print("Este o zi calduroasa")
+#     print("Bea multa apa")
+# elif is_cold:
+#     print("Este o zi rece.")
+#     print('Pune o geaca pe tine.')
+# else:
+#     print("Este o zi placuta")
+#
+# print("Distractie placuta")
+
+pret_casa = 25000
+are_credit_bun = False
+
+if are_credit_bun:
+    avans = 0.1 * pret_casa
+else:
+    avans = 0.2 * pret_casa
+print(f"Avans : {avans}")
---
+++ modul2/exercitii acasa 4.py
@@ -0,0 +1,4 @@
+firts_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+result = float(firts_number) + float(second_number)
+print(result)
