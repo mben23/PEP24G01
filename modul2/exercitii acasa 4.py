72d0b89574248edbbc6fabddaf487a552cce76cb
mben23 <bmarisescu@gmail.com>
2024-02-26 14:25:08 +0200

    test github

--- modul2/exercitii acasa 4.py
+++ modul2/exercitii acasa 4.py
@@ -1,4 +1,12 @@
-i = 1
-while i <= 20:
-    print(i)
-    i = i + 2
+# preturi = [10, 30, 40]
+#
+# total = 0
+# for pret in preturi:
+#     total += pret
+# print(f"Total: {total}")
+
+# Nested loops
+
+for x in range(4):
+    for y in range(4):
+        print(f'({x}, {y})')
